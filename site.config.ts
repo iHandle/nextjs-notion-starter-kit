6dfd862ae42d843b5b4c41261ce659d43e7f4bf0
iHandle <mail@ihandle.top>
2022-04-14 00:32:41 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,16 +9,16 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Jane❤️iHandle',
+  domain: 'jane.ihandle.top',
+  author: 'Jane❤️iHandle',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
 //   twitter: 'transitive_bs',
-//   github: 'transitive-bullshit',
+  github: 'ihandle',
 //   linkedin: 'fisch2',
 
   // default notion icon and cover images for site-wide consistency (optional)
