0a03f192dd3054b0e46cfbc86ac50a0205072cad
iHandle <mail@ihandle.top>
2022-04-14 17:53:04 +0800

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -30,7 +30,8 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2022 {config.author}</div>
+      {/* <div className={styles.copyright}>Copyright 2022 {config.author}</div> */}
+      <div className={styles.copyright}></div>
 
       <div className={styles.settings}>
         {hasMounted && (
